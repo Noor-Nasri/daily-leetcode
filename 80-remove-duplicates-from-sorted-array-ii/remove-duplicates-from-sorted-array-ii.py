c15ae3b9a0319df4948732829e2c36d6c481614e
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-05 07:07:08 -0400

    Time: 48 ms (79.16%) | Memory: 16.5 MB (76.65%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -2,18 +2,18 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         num_included = 0
         last_number = None
-        seen_once = True
+        seen_twice = False
 
         for val in nums:
             if val != last_number:
                 nums[num_included] = val
-                seen_once = True
+                seen_twice = False
                 last_number = val
                 num_included += 1
     
-            elif seen_once:
+            elif not seen_twice:
                 nums[num_included] = val
-                seen_once = False
+                seen_twice = True
                 num_included += 1
         
         return num_included
