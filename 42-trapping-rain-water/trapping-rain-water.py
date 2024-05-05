fbb5eedb665b72a10768dcb52704ccb7efae5e7c
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-05 07:34:04 -0400

    Time: 109 ms (37.33%) | Memory: 19.3 MB (5.18%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,25 +1,21 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
-        tallest_from_left = [-1 for _ in range(n)]
-        tallest_from_right = [-1 for _ in range(n)]
-        tallest_L = 0
-        tallest_R = 0
+        tallest_from_right = {}
+        tallest_R = -1
 
-        for ind in range(n):
-            tallest_L = max(height[ind], tallest_L)
-            tallest_R = max(height[n - 1 - ind], tallest_R)
-
-            tallest_from_left[ind] = tallest_L
-            tallest_from_right[n - 1 - ind] = tallest_R
+        for ind in range(n - 1, -1, -1):
+            tallest_R = max(tallest_R, height[ind])
+            tallest_from_right[ind] = tallest_R
         
         total = 0
+        tallest_from_left = height[0]
         for ind in range(1, n - 1):
             val = height[ind]
-            fill = min(tallest_from_left[ind - 1], tallest_from_right[ind + 1])
+            fill = min(tallest_from_left, tallest_from_right[ind + 1])
             if val < fill:
                 total += fill - val
-
+            tallest_from_left = max(val, tallest_from_left)
         return total
         
 
