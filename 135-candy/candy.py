86bf3bbad38a24618d8559692df0903672f63667
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-06 07:02:24 -0400

    Time: 115 ms (92.31%) | Memory: 19.4 MB (62.61%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,13 +1,13 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        assignments = [1 for i in range(len(ratings))]
-        ind = 1
+        candy = len(ratings) # start at 1 per person, increase as required
         n = len(ratings)
-
+        ind = 1
+        last_value = 1 # number of candies given at [ind - 1]
 
         while (ind < n):
             if (ratings[ind] < ratings[ind - 1]): 
-                # Must be smaller than prev, first see how far the chain goes
+                # MUST be smaller than prev, first see how far the chain goes
                 chain_end = ind + 1
                 while (chain_end < n):
                     if (ratings[chain_end] < ratings[chain_end - 1]):
@@ -15,22 +15,28 @@ class Solution:
                     else:
                         break
 
-                # Now go backwards to set them
-                # This is still O(n) overall because chains dont overlap
-                if chain_end - ind > 1:
-                    for backward_ind in range(chain_end - 2, ind - 1, -1):
-                        assignments[backward_ind] = assignments[backward_ind + 1] + 1
-                
+                # This means, this chain takes form [... 3 2 1]
+                # Total extra candy, excluding ind-1, is .. + 2 + 1
+                sequence_length = chain_end - ind - 1
+                candy += sequence_length * (1 + sequence_length) / 2
+                                
                 # Finally, resolve that ind must be smaller than ind-1
-                assignments[ind - 1] = max(assignments[ind - 1], assignments[ind] + 1)
+                # That means, ind-1 must be bigger than ind
+                if (last_value <= sequence_length + 1):
+                    candy += sequence_length + 2 - last_value
+
                 ind = chain_end
+                last_value = 1
             else:
-                # Check if we need to increase, otherwise keep at 1 and go next
-                if (ratings[ind] > ratings[ind - 1] and assignments[ind] <= assignments[ind - 1]):
-                    assignments[ind] = assignments[ind - 1] + 1 
-                    
+                if (ratings[ind] > ratings[ind - 1]):
+                    # MUST give another X candy to this one
+                    candy += last_value
+                    last_value+=1
+                else:
+                    last_value=1
                 ind += 1 
-        return sum(assignments)
+
+        return int(candy)
 
 
         
