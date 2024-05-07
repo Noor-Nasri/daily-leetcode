b3b357d38478f1916d12e883e1635283402ce2fb
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-07 05:03:20 -0400

    Time: 48 ms (48.24%) | Memory: 16.6 MB (19.26%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,12 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        total = 0
         valueMappings = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        lastValue = valueMappings[s[0]]
+        lastValue = 0 #valueMappings[s[0]]
+        total = 0
 
-        for i in range(1, len(s)):
-            val = valueMappings[s[i]]
-
+        for c in s:
+            val = valueMappings[c]
+            
             if val <= lastValue:
                 total += lastValue
             else:
