c921b8a567cea63dc008b55c22ddba9f8a42e939
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-07 01:53:10 -0400

    Time: 137 ms (8.56%) | Memory: 20.5 MB (73.38%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -8,7 +8,8 @@ class Solution:
         for i in range(1, len(intervals)):
             if (intervals[i][0] <= cur[1]):
                 # Meeting started before last ended, extend
-                cur[1] = max(cur[1], intervals[i][1])
+                if intervals[i][1] > cur[1]:
+                    cur[1] = intervals[i][1]
             else:
                 merged.append(cur)
                 cur = intervals[i]
