8a7eb96a3bb22e1db499475d47b5699d35ff6511
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-06 23:08:11 -0400

    Time: 60 ms (89.31%) | Memory: 16.7 MB (86.47%) - LeetSync

---
+++ 149-max-points-on-a-line/max-points-on-a-line.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) < 2: return len(points)
+
+        bestNum = 2
+        points = sorted(points, key = lambda x: x[0])
+
+        for i1 in range(len(points)):
+            p1 = points[i1]
+            lines = {}
+            for i2 in range(i1 + 1, len(points)):
+                p2 = points[i2]
+
+                if (p1[0] == p2[0]):
+                    m = 'inf'
+                else:
+                    m = (p2[1] - p1[1])/(p2[0] - p1[0])
+                
+                if i1 == 0: print(m)
+
+                if m in lines:
+                    lines[m] += 1
+
+                    if lines[m] > bestNum:
+                        bestNum = lines[m]
+                else:
+                    lines[m] = 2
+
+        return bestNum
