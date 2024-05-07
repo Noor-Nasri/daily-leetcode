64d4dbe13fc96a3dbb781fe7221db286db6888a4
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-07 01:52:07 -0400

    Time: 127 ms (38.45%) | Memory: 20.6 MB (54.20%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,31 +1,17 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        values = [0 for i in range(10001)]
-        singles = set()
+        intervals = sorted(intervals, key = lambda x: x[0])
 
-        for interval in intervals:
-            if interval[0] == interval[1]:
-                # edge case
-                singles.add(interval[0])
-            else:
-                values[interval[0]] += 1
-                values[interval[1]] -= 1
-        
         merged = []
-        cur_sum = 0
-        cur_start = -1
-        for ind in range(len(values)):
-            cur_sum += values[ind]
-            if (cur_sum > 0 and cur_start == -1):
-                # Started a new interval
-                cur_start = ind
-            elif (cur_sum == 0 and cur_start > -1):
-                # Finished an old interval
-                merged.append([cur_start, ind])
-                cur_start = -1
-            elif (cur_sum == 0 and ind in singles):
-                # Must include (inc, inc) since it was provided
-                merged.append([ind, ind])
+        cur = intervals[0]
 
+        for i in range(1, len(intervals)):
+            if (intervals[i][0] <= cur[1]):
+                # Meeting started before last ended, extend
+                cur[1] = max(cur[1], intervals[i][1])
+            else:
+                merged.append(cur)
+                cur = intervals[i]
+        
+        merged.append(cur)
         return merged
-
