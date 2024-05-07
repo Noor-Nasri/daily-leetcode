6eb3f7b32549b7eb192c1047aab8b392c8d33ecb
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-06 21:17:35 -0400

    Time: 15 ms (60.55%) | Memory: 11.2 MB (34.44%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<char, int> magazine_counts;
         for (char c : magazine) magazine_counts[c]++;
         for (char c : ransomNote){
-            if (!magazine_counts[c]) return false;
+            if (magazine_counts[c] == 0) return false;
             magazine_counts[c]--;
         }
 
