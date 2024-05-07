ea5c7c6e340d08a8d6041839ffc955e5af2ea62f
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-06 21:15:55 -0400

    Time: 15 ms (60.55%) | Memory: 11.2 MB (53.46%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -3,7 +3,7 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char, int> magazine_counts;
         
-        for (char c = 'a'; c <= 'z'; c++) magazine_counts[c] = 0;
+        //for (char c = 'a'; c <= 'z'; c++) magazine_counts[c] = 0;
         for (char c : magazine) magazine_counts[c]++;
 
         for (char c : ransomNote){
