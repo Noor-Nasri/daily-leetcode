945e24a6a2d8f02fdb98c7ccab26153adece2f03
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-08 03:24:35 -0400

    Time: 36 ms (57.16%) | Memory: 16.6 MB (14.48%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -2,11 +2,14 @@ class Solution:
     def reverseBits(self, n: int) -> int:
 
         total = 0
-        for i in range(32, -1, -1):
-            val = 2**i
-            if (n // val):
-                n -= val
-                total += 2**(32 - i - 1)
+        bit_power = 2**31
+        reverse_power = 1
+        for i in range(32):
+            if (n // bit_power):
+                n -= bit_power
+                total += reverse_power
+            bit_power /= 2
+            reverse_power *= 2
         
         return total
         
