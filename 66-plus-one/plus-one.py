995a5be818761426b8e76497d03ba80d389a40d5
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-08 04:39:48 -0400

    Time: 41 ms (22.62%) | Memory: 16.7 MB (10.42%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -3,11 +3,9 @@ class Solution:
         for i in range(len(digits) -1, -1, -1):
             if (digits[i] < 9):
                 digits[i] += 1
-                break
+                return digits
             
             digits[i] = 0
-            if (i == 0):
-                return [1] + digits
 
-        return digits
+        return [1] + digits
         
