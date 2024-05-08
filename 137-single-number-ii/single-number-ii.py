0b6954cd657e32503381658392d39262275818e3
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-08 04:07:46 -0400

    Time: 87 ms (25.32%) | Memory: 18.7 MB (57.12%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,16 +1,23 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # not sure how to do bit manipulation
-        # will instead just use a set with minimal memory, then research
+        # Originally did this question with a minimal set (beats 80%)
+        # After reading the bit-manipulation ideas, I adjusted this
+        # We want to count number of 1s across each bit
+        # When the real bit is 1, the total has remainder 1 (from %3)
 
-        seen = set()
-        total_unique = 0
-        total_all = 0
-        for num in nums:
-            total_all += num
-            if num in seen: continue
-            seen.add(num)
-            total_unique += num
-        
-        return (total_unique*3 - total_all)//2
+        total = 0
+        for i in range(32):
+            num_1s = 0
+            for num in nums:
+                # num >> i shifts the binary i times to the RIGHT
+                # that means, the wanted one will be at the last position
+                # ie least significant. So then we AND with 1 to just get the last
+                is_1 = (num >> i) & 1
+                num_1s += is_1
+
+            if num_1s % 3:
+                total += 2**i
+
+        if total >= 2**31: total -= 2**32
+        return total
         
