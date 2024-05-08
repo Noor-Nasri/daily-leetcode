b29e98fc799ab0a831c990ec158b9e0a636f8095
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-08 03:06:39 -0400

    Time: 42 ms (76.44%) | Memory: 42.3 MB (25.49%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        // idea is simple: circular means sum(0:i) + sum (j:n) where i<=j
+        // idea 1: circular means sum(0:i) + sum (j:n) where i<=j
         // first solve with greedy (no circular), then enforce circular
 
-        // greedy for non-circ: just count best and reset when needed
-        int n = nums.size();
-        int best_non_circ = nums[0];
-        int cur = 0;
-        for (int i = 0; i < n; i++){
-            cur += nums[i];
-            if (cur > best_non_circ) best_non_circ = cur;
-            if (cur < 0) cur = 0;
+        // That solution ran at 44ms (beats 64%), but then I saw another method
+        // Instead of enforcing circular, notice that max_circ = total - min_arr
+
+        // solve both with greedy, then take best
+        int best_non_circ = nums[0]; // forced non-empty
+        int best_minarr = 100000;
+
+        int cur_max = 0;
+        int cur_min = 0;
+        int total = 0;
+        for (int i = 0; i < nums.size(); i++){
+            cur_max += nums[i];
+            cur_min += nums[i];
+            total += nums[i];
+
+            if (cur_max > best_non_circ) best_non_circ = cur_max;
+            if (cur_min < best_minarr) best_minarr = cur_min;
+
+            if (cur_max < 0) cur_max = 0;
+            if (cur_min > 0) cur_min = 0;
         }
 
-        if (best_non_circ < 0){ // all negatives edge case
+        if (best_non_circ < 0){ 
+            // all negatives edge case
             return best_non_circ;
         }
 
-        // now circular: store greedy for both directions then combine
-        int best_until[n];
-        int best_from[n];
-        best_until[0] = nums[0]; // non-empty
-        best_from[n-1] = nums[n-1];
-        int total_forward = nums[0];
-        int total_backward = nums[n-1];
-
-        for (int i = 1; i < n; i++){
-            total_forward += nums[i];
-            total_backward += nums[n - 1 - i];
-
-            best_until[i] = max(best_until[i-1], total_forward);
-            best_from[n - 1 - i] = max(best_from[n - i], total_backward);
-        }
-
-        // combine the arrays
-        int best_circ = 0;
-        for (int i = 0; i < n - 2; i++){
-            int option = best_until[i] + best_from[i + 2];
-            if (option > best_circ) best_circ = option;
-        }
-
-        return max(best_circ, best_non_circ);
+        int best_circ = total - best_minarr;
+        return max(best_non_circ, best_circ);
     }
 };
