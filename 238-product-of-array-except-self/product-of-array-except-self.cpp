c24227db0c27c6c0030bd260e1ffa947c4488614
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-08 00:20:23 -0400

    Time: 23 ms (28.80%) | Memory: 38.1 MB (22.77%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> solution;
+        vector<int> solution(nums.size());
         // This one took too long because I was trying to do math ..
         // The idea is super simple: ABC [D] EF = (ABD)x(EF)=
         // build up (ABD) then go backward to get (EF)
 
         int prod = 1;
-        for (int num: nums){
-            solution.push_back(prod);
-            prod *= num;
+        for (int i = 0; i < nums.size(); i++){
+            solution[i] = prod;
+            prod *= nums[i];
         }
 
         // backward pass
