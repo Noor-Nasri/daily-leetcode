0611edb3fbfbf840561d23d9e8805624092e3a4b
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-08 22:03:17 -0400

    Time: 4 ms (30.86%) | Memory: 7.2 MB (82.26%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int hammingWeight(int n) {
+        /*
         int numOnes = 0;
         for (int i = 30; i > -1; i--){
             int cutoff = pow(2, i);
@@ -10,6 +11,16 @@ public:
             }
         }
 
+        return numOnes;
+        */
+
+        // Above solution ran at 0ms, now looking at bitwise for fun
+        // Turned out super easy, just check is LSB is 1 then shift
+        int numOnes = 0;
+        while (n){
+            numOnes += (n & 1);
+            n >>= 1;
+        }
         return numOnes;
     }
 };
