63d0b4b145e60f642ed6f54f5caad5d6dc863cf3
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-25 21:40:58 -0400

    Time: 43 ms (96.00%) | Memory: 16.5 MB (95.70%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -2,9 +2,7 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # Store last index. Once we see it again, discard those indices
         # Thus O(n) going forward, and O(n) total of removals possible. Thus O(n)
-    
         if len(s) == 0: return 0
-
 
         bestCount = 0
         curCount = 1
@@ -16,14 +14,12 @@ class Solution:
             if not (c in seen):
                 seen[c] = i
                 curCount += 1
-                print("Included", c, "now we have", curCount)
                 continue
 
             # gredy: remove the ones that make including this impossible
             if curCount > bestCount:
                 bestCount = curCount
             
-            print("Prev index:", seen[c], "cur index:", i)
             prev_index = seen[c]
             curCount = i - prev_index
             for rem_i in range(last_start, prev_index):
@@ -31,7 +27,5 @@ class Solution:
             
             seen[c] = i
             last_start = prev_index + 1
-            print("Reset by", c, "now we have", curCount)
 
-        
         return max(curCount, bestCount)
