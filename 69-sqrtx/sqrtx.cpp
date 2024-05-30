a4fbb4a70a3a3120c75be323da913dc4a7775318
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-30 14:22:00 -0400

    Time: 14 ms (14.23%) | Memory: 7.4 MB (80.65%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -3,8 +3,7 @@ public:
     int mySqrt(int x) {
         for (int i = 1; i <= x; i++){
             long num = i;
-            long prod = num*num;
-            if (prod > x) return i-1;
+            if (num*num > x) return i-1;
         }
 
         return x;
