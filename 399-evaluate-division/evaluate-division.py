5c87b162280c5bec2908fbc678664950609fa239
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-31 01:12:23 -0400

    Time: 43 ms (11.41%) | Memory: 16.7 MB (52.18%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -5,18 +5,15 @@ class Solution:
         def found(num, denom, val):
             if denom in divisions[num]: return # already done
 
-            # Exact one
+            # Known already
             divisions[num][denom] = val
             if num == denom: return # cant get anything out of x/x=1
-
-            # Reciprical
             if val != 0: divisions[denom][num] = 1/val
             
             # Get info by eliminating denom
             oldDenoms = list(divisions[denom].keys())
             for newDenom in oldDenoms:
                 newVal = val * divisions[denom][newDenom]
-                #print("From", num, denom, val, "we found new pair", num, newDenom, newVal)
                 found(num, newDenom, newVal)
 
             # Get info by eliminating num
@@ -24,24 +21,17 @@ class Solution:
             for newNum in oldNums:
                 if divisions[num][newNum] == 0: continue
                 newVal = val / divisions[num][newNum]
-                #print("From", num, denom, val, "we found new pair", newNum, denom, newVal)
                 found(newNum, denom, newVal)
 
 
         for ind in range(len(equations)):
-            #print("Round", ind)
             num, denom = equations[ind]
             val = values[ind]
 
             if not num in divisions: divisions[num] = {}
             if not denom in divisions: divisions[denom] = {}
-
             found(num, denom, val)
-            
-            
-            #print(divisions)
-            #print("===")
-        
+
         answers = []
         for a, b in queries:
             if not a in divisions or not b in divisions[a]: 
