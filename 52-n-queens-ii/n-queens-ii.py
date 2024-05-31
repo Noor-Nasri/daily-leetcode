e107b1c6445c2c25fff5910ee2c7cbc167467dfb
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-31 15:31:16 -0400

    Time: 2092 ms (5.01%) | Memory: 16.5 MB (38.91%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.py
+++ 52-n-queens-ii/n-queens-ii.py
@@ -1,44 +1,49 @@
 class Solution:
-    def recurSolver(self, nRem, board, curRow, curCol):
+    def recurSolver(self, nRem, board, curRow, curCol, availRem):
         if nRem == 0:
-            #print("Found solution!")
-            #print("\n".join([" ".join(str(e)) for e in board] ))
             return 1
+        if availRem <= 0:
+            return 0
 
         if curCol == len(board):
             curCol = 0
             curRow += 1
-            if curRow == len(board): 
-                return 0
         
         if board[curRow][curCol]: # Cant place here
-            return self.recurSolver(nRem, board, curRow, curCol + 1)
+            return self.recurSolver(nRem, board, curRow, curCol + 1, availRem)
         
+        availRem -= 1 # This tile is no longer avail in the future
         # Case 1: Ignore this tile
-        case1 = self.recurSolver(nRem, board, curRow, curCol + 1)
+        case1 = self.recurSolver(nRem, board, curRow, curCol + 1, availRem)
 
         # Case 2: put a queen here
         board2 = [e[:] for e in board]
 
         # Horizontal, Vertical, then Diagonal
         for newCol in range(curCol + 1, len(board)):
-            board2[curRow][newCol] = 1
+            if not board2[curRow][newCol]:
+                board2[curRow][newCol] = 1
+                availRem -= 1
         
         for newRow in range(curRow + 1, len(board)):
-            board2[newRow][curCol] = 1
+            if not board2[newRow][curCol]:
+                board2[newRow][curCol] = 1
+                availRem -= 1
         
         for diag in range(1, len(board) - curRow):
-            if curCol + diag < len(board):
+            if curCol + diag < len(board) and not board2[curRow + diag][curCol + diag]:
                 board2[curRow + diag][curCol + diag] = 1
-            if curCol - diag >= 0:
+                availRem -= 1
+
+            if curCol - diag >= 0 and not board2[curRow + diag][curCol - diag]:
                 board2[curRow + diag][curCol - diag] = 1
+                availRem -= 1
         
         board2[curRow][curCol] = 2
-        case2 = self.recurSolver(nRem - 1, board2, curRow, curCol + 1)
-        
+        case2 = self.recurSolver(nRem - 1, board2, curRow, curCol + 1, availRem)
         return case1 + case2
 
 
     def totalNQueens(self, n: int) -> int:
-        return self.recurSolver(n, [[0 for i in range(n)] for e in range(n)], 0, 0)
+        return self.recurSolver(n, [[0 for i in range(n)] for e in range(n)], 0, 0, n**2)
         
