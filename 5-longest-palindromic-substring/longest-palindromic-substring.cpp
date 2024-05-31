5ba2eaaa8247fcf9ee78ae469dd6b8d8668627f9
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-31 02:22:48 -0400

    Time: 79 ms (48.01%) | Memory: 8.3 MB (74.32%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -6,45 +6,45 @@ public:
         int curSize = 2;
         bool isPalindrome1[s.size()];
         bool isPalindrome2[s.size()];
+        bool nextLayer[s.size()];
         int lastFound = 1;
 
         // initialize
         for (int i = 0; i < s.size(); i++){
             isPalindrome1[i] = true;
-            if (i < s.size() - 1 && s[i] == s[i+1]){
-                isPalindrome2[i] = true;
-                lastFound = 2; 
-            }else{
-                isPalindrome2[i] = false;
-            }    
+            nextLayer[i] = false;
+            if (i < s.size() - 1){
+                if (s[i] == s[i+1]){
+                    isPalindrome2[i] = true;
+                    lastFound = 2; 
+                }else{
+                    isPalindrome2[i] = false;
+                }
+            }   
         }
 
+        // Terminates when can't find palindrome in 2 conseq numbers
         while (curSize < s.size()){
-            // At least one current palindrome
-            bool nextLayer[s.size()];
-            for (int i = 0; i < s.size(); i++) nextLayer[i] = false;
-
-            //cout << s.size() << " " << curSize << '\n';
             for (int i = 0; i < s.size() - curSize; i++){
-                //cout << i << ": " << s[i] << "=" << s[i + curSize] << ", " << isPalindrome1[i + 1] << '\n';
                 if (s[i] == s[i + curSize] && isPalindrome1[i + 1]){
                     nextLayer[i] = true;
                     lastFound = curSize+1;
-                    //cout << "Updated found to " << lastFound << " due to " << i << '\n';
+                } else{
+                    nextLayer[i] = false;
                 }
             }
 
             // set vars for next iteration
             curSize++;
             if (curSize - lastFound >= 2) break;
-            for (int i = 0; i < s.size(); i++){
+            for (int i = 0; i < s.size() - lastFound + 1; i++){
                 isPalindrome1[i] = isPalindrome2[i];
                 isPalindrome2[i] = nextLayer[i];
             }
         }
 
         // return part of longest sequence
-        for (int i = 0; i < s.size(); i++){
+        for (int i = 0; i < s.size() - lastFound + 1; i++){
             if (lastFound == curSize && isPalindrome2[i]
                 || lastFound < curSize && isPalindrome1[i]){
                 return s.substr(i, lastFound); 
