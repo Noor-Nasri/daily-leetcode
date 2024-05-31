8e1446ebc38db175a76c56e26704e4fd88b1ee26
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-05-31 02:17:36 -0400

    Time: 142 ms (32.94%) | Memory: 8.1 MB (83.19%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -4,31 +4,31 @@ public:
         if (s.size() < 2) return s;
         // Start off with size 1 and 2 palindromes, then keep merging upwards
         int curSize = 2;
-        int isPalindrome1[s.size()];
-        int isPalindrome2[s.size()];
+        bool isPalindrome1[s.size()];
+        bool isPalindrome2[s.size()];
         int lastFound = 1;
 
         // initialize
         for (int i = 0; i < s.size(); i++){
-            isPalindrome1[i] = 1;
+            isPalindrome1[i] = true;
             if (i < s.size() - 1 && s[i] == s[i+1]){
-                isPalindrome2[i] = 1;
+                isPalindrome2[i] = true;
                 lastFound = 2; 
             }else{
-                isPalindrome2[i] = 0;
+                isPalindrome2[i] = false;
             }    
         }
 
         while (curSize < s.size()){
             // At least one current palindrome
-            int nextLayer[s.size()];
-            for (int i = 0; i < s.size(); i++) nextLayer[i] = 0;
+            bool nextLayer[s.size()];
+            for (int i = 0; i < s.size(); i++) nextLayer[i] = false;
 
             //cout << s.size() << " " << curSize << '\n';
             for (int i = 0; i < s.size() - curSize; i++){
                 //cout << i << ": " << s[i] << "=" << s[i + curSize] << ", " << isPalindrome1[i + 1] << '\n';
                 if (s[i] == s[i + curSize] && isPalindrome1[i + 1]){
-                    nextLayer[i] = 1;
+                    nextLayer[i] = true;
                     lastFound = curSize+1;
                     //cout << "Updated found to " << lastFound << " due to " << i << '\n';
                 }
