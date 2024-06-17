616b91ac4d29a409b376de66dbcf6cc90ebab02c
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-06-17 00:29:19 -0400

    Time: 11 ms (11.90%) | Memory: 7.4 MB (18.45%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool judgeSquareSum(int c) {
         if (c == 0) return true;
+
         double cutoff = pow(c, 0.5);
         for (int a = 0; a < cutoff; a++){
             double b = pow(c - pow(a, 2), 0.5);
-            int int_b = b;
-            if (b == int_b){ // suitable choice for b
+            if (b == floor(b)){ // suitable choice for b
                 return true;
             }
         }
