450c04e1c258725b2f28840efa3137fee0d2e120
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-06-20 01:26:18 -0400

    Time: 141 ms (9.97%) | Memory: 61.3 MB (73.97%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canSolve(vector<int>& position, int m, int min_dist){
+        int rem = m - 1;
+        int earliest = position[0] + min_dist;
+        int ind = 1;
+
+        while (rem > 0 && ind < position.size()){
+            if (position[ind] >= earliest){
+                rem--;
+                earliest = position[ind] + min_dist;
+            }
+            ind++;
+        }
+
+        return rem==0;
+    }
+
+
+    int maxDistance(vector<int>& position, int m) {
+        // Idea: for a specific <min dist>, can check solution in O(n)
+        // Just put ball in earliest and skip the next <dist>, continue
+        // Combine this with binary search, it is O(nlogm). Sort is O(nlogn)
+
+        sort(position.begin(), position.end());
+        int low = 1;
+        int high = position[position.size()-1] - position[0];
+
+        while (low <= high){
+            int mid = (low + high + 1)/2;
+
+            if (canSolve(position, m, mid)){
+                if (low == high){
+                    return mid;
+                }
+
+                low = mid;
+            }else{
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
