abbdd5d34ea8145887f0330d461e06d318e0d22d
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-08-06 00:41:08 -0700

    Time: 140 ms (89.26%) | Memory: 17.7 MB (36.75%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,16 +1,13 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        letter_counts = [0 for i in range(26)]
-        for let in word:
-            letter_counts[ord(let) - 97] += 1
-        
-        
-        letter_counts = sorted(letter_counts, reverse = True)
+        letter_counts = Counter(word)
+        key_order = sorted(letter_counts.keys(), reverse = True, key = lambda e : letter_counts[e])
         costs = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4]
         total_pushes = 0
         
         ind_cost = 0
-        for ind in range(26):
-            total_pushes += costs[ind]*letter_counts[ind]
+        for key in key_order:
+            total_pushes += costs[ind_cost]*letter_counts[key]
+            ind_cost += 1
         
         return total_pushes
