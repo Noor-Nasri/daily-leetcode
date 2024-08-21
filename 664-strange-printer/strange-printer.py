bcc86eb4ca45baf6832e130bdf4339acb8561b84
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-08-21 00:25:51 -0700

    Time: 330 ms (64.10%) | Memory: 16.9 MB (43.59%) - LeetSync

--- 664-strange-printer/strange-printer.py
+++ 664-strange-printer/strange-printer.py
@@ -8,18 +8,17 @@ class Solution:
         if start >= end or start >= len(self.s):
             return 0
 
-        if solved[ord(self.s[start]) - ord('a')]:
+        relevantChar = self.s[start]
+
+        if relevantChar == solved:
             return self.complexSolver(start + 1, end, solved)
 
         best = 10000000
-
         for possible_end in range(start, end):
-            if self.s[start] != self.s[possible_end]: continue
+            if relevantChar != self.s[possible_end]: continue
 
             # we can possibly make this adjustment
-            solved_inside = [0 for i in range(26)]
-            solved_inside[ord(self.s[start]) - ord('a')] = 1
-            cost_inside = self.complexSolver(start + 1, possible_end, tuple(solved_inside))
+            cost_inside = self.complexSolver(start + 1, possible_end, relevantChar)
             cost_outside = self.complexSolver(possible_end + 1, end, solved)
             best = min(best, 1 + cost_inside + cost_outside)
 
@@ -30,6 +29,6 @@ class Solution:
     def strangePrinter(self, s: str) -> int:
         self.sols = {}
         self.s = s
-        return self.complexSolver(0, len(s), tuple(0 for i in range(26)))
+        return self.complexSolver(0, len(s), '')
         
         
