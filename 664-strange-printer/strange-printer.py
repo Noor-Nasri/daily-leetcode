397b6e78e9e6e19ef73f5278ed9b444c93eeae2f
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-08-21 00:27:48 -0700

    Time: 219 ms (83.88%) | Memory: 16.9 MB (42.49%) - LeetSync

--- 664-strange-printer/strange-printer.py
+++ 664-strange-printer/strange-printer.py
@@ -7,20 +7,28 @@ class Solution:
         
         if start >= end or start >= len(self.s):
             return 0
+        
 
         relevantChar = self.s[start]
 
-        if relevantChar == solved:
-            return self.complexSolver(start + 1, end, solved)
-
+        
         best = 10000000
-        for possible_end in range(start, end):
-            if relevantChar != self.s[possible_end]: continue
+        if relevantChar == solved:
+            best = self.complexSolver(start + 1, end, solved)
+        
+        elif relevantChar == self.s[end - 1]:
+            # Greedy closing boundary
+            best = 1 + self.complexSolver(start + 1, end - 1, relevantChar)
+        
+        else:
 
-            # we can possibly make this adjustment
-            cost_inside = self.complexSolver(start + 1, possible_end, relevantChar)
-            cost_outside = self.complexSolver(possible_end + 1, end, solved)
-            best = min(best, 1 + cost_inside + cost_outside)
+            for possible_end in range(start, end):
+                if relevantChar != self.s[possible_end]: continue
+
+                # we can possibly make this adjustment
+                cost_inside = self.complexSolver(start + 1, possible_end, relevantChar)
+                cost_outside = self.complexSolver(possible_end + 1, end, solved)
+                best = min(best, 1 + cost_inside + cost_outside)
 
         self.sols[(start, end, solved)] = best
         return best
