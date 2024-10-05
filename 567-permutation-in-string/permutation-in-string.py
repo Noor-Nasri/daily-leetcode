f3643623f3ec8c0d90c8ef4c3476d02425e764c3
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-10-04 18:17:37 -0700

    Time: 52 ms (82.47%) | Memory: 16.6 MB (62.60%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -4,16 +4,16 @@ class Solution:
 
         letters = [0 for i in range(26)]
         for let in s1:
-            letters[ord(let) - ord('a')] += 1
+            letters[ord(let) - 97] += 1
         
         ind = 0
         while (ind < len(s2)):
             let = s2[ind]
-            letters[ord(let) - ord('a')] -= 1
+            letters[ord(let) - 97] -= 1
 
             if ind >= len(s1): # shift the window
                 old_let = s2[ind - len(s1)]
-                letters[ord(old_let) - ord('a')] += 1
+                letters[ord(old_let) - 97] += 1
 
             if not any(letters):
                 return True
