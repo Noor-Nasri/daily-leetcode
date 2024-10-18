a6a2aebaf12f2ba59f859a34318181ebca782350
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-10-17 22:43:54 -0700

    Time: 6 ms (100.00%) | Memory: 16.8 MB (32.63%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -1,14 +1,22 @@
 class Solution:
-    # constraints are n <= 16, I am tired so I'll just brute force
+    # constraints are n <= 16, I am tired so I'll just brute force with gimmicky DP
     def bruteForceSolve(self, currentVal, index):
         if index == len(self.nums):
             return int(currentVal == self.maxVal)
+
+        if (currentVal, index) in self.sols:
+            return self.sols[(currentVal, index)] 
+
         
-        return self.bruteForceSolve(currentVal, index + 1) + self.bruteForceSolve(currentVal | self.nums[index], index + 1)
+        
+        ans = self.bruteForceSolve(currentVal, index + 1) + self.bruteForceSolve(currentVal | self.nums[index], index + 1)
+        self.sols[(currentVal, index)] = ans
+        return ans
 
     def countMaxOrSubsets(self, nums: List[int]) -> int:
         self.nums = nums
         self.maxVal = 0
+        self.sols = {}
         for num in nums:
             self.maxVal |= num
 
