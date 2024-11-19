6711a90193957a0adfdb63335906dd84eb368808
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-11-18 21:35:50 -0800

    Time: 177 ms (15.14%) | Memory: 36.4 MB (21.72%) - LeetSync

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -0,0 +1,35 @@
+class Solution:
+    def addNum(self, num):
+        if num in self.seen:
+            self.seen[num] += 1
+            self.duplicatedKeys.add(num)
+        else:
+            self.seen[num] = 1
+        self.total += num
+
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        maxTotal = 0
+        self.total = 0
+        self.seen = {}
+        self.duplicatedKeys = set()
+
+
+        for i in range(k - 1):
+            self.addNum(nums[i])
+
+        for ind in range(k - 1, len(nums)):
+            self.addNum(nums[ind])
+            if not self.duplicatedKeys:
+                maxTotal = max(maxTotal, self.total)
+            
+            numToExclude = nums[ind - k + 1]
+            self.seen[numToExclude] -= 1
+            self.total -= numToExclude
+            if self.seen[numToExclude] == 1:
+                self.duplicatedKeys.remove(numToExclude)
+            elif self.seen[numToExclude] == 0:
+                print("Now deleting it")
+                del self.seen[numToExclude]
+        
+        return maxTotal
+
