ceefaf8582d30e188d67e37047003e11b6dc2ea0
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2024-12-25 17:50:35 -0800

    Time: 79 ms (85.35%) | Memory: 18 MB (63.39%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -5,13 +5,23 @@ class Solution:
 
         if ind == len(nums):
             return int(target == curSum)
-        
-        sol = self.findTarget(nums, target, ind + 1, curSum + nums[ind])
-        sol += self.findTarget(nums, target, ind + 1, curSum - nums[ind])
+
+        if curSum + self.sums[ind] < target:
+            sol = 0
+        elif curSum - self.sums[ind] > target:
+            sol = 0
+        else:
+            sol = self.findTarget(nums, target, ind + 1, curSum + nums[ind])
+            sol += self.findTarget(nums, target, ind + 1, curSum - nums[ind])
+            
         self.sols[(ind, curSum)] = sol
         return sol
 
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         self.sols = {}
+        self.sums = [sum(nums)]
+        for ind in range(len(nums)):
+            self.sums.append(self.sums[-1] - nums[ind])
+
         return self.findTarget(nums, target, 0, 0)
         
