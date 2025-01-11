15d61f49dd566ebea05dc8290972bbc9b91b53ee
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2025-01-10 21:48:09 -0800

    Time: 50 ms (26.43%) | Memory: 18.2 MB (16.30%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        counts = [0 for i in range(26)]
+        for c in s:
+            counts[ord(c) - 97] += 1
+        
+        print(counts)
+
+        longestPalindrome = 0
+        addedCenter = 0
+
+        for ind in range(26):
+            if not counts[ind]:
+                continue
+            
+            if counts[ind] % 2 == 0:
+                longestPalindrome += counts[ind]
+            elif not addedCenter:
+                addedCenter = True
+                longestPalindrome += counts[ind]
+            else:
+                longestPalindrome += counts[ind] - 1
+        
+        remainingChars = len(s) - longestPalindrome
+
+        if remainingChars >= k:
+            # We have too many single chars that make their own palindrome 
+            return False
+        
+        extraPalindromesNeeded = k - 1 - remainingChars
+        numExtraPossible = (longestPalindrome - 1) // 2 * 2
+        return numExtraPossible >= extraPalindromesNeeded
+
+        
