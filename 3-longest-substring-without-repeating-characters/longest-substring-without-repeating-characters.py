e388776501c2be704325772c3171fce0c9e60085
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2025-05-27 18:51:28 -0700

    Time: 20 ms (40.24%) | Memory: 17.8 MB (82.16%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,31 +1,18 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # Store last index. Once we see it again, discard those indices
-        # Thus O(n) going forward, and O(n) total of removals possible. Thus O(n)
-        if len(s) == 0: return 0
+        seen_indices = {}
+        left = 0
+        maxLen = 0
 
-        bestCount = 0
-        curCount = 1
-        seen = {s[0] : 0} # char -> index
-        last_start = 0
-
-        for i in range(1, len(s)):
-            c = s[i]
-            if not (c in seen):
-                seen[c] = i
-                curCount += 1
-                continue
-
-            # gredy: remove the ones that make including this impossible
-            if curCount > bestCount:
-                bestCount = curCount
+        for right in range(len(s)):
+            char = s[right]
+            if char in seen_indices:
+                for ind_prev in range(left, seen_indices[char]):
+                    del seen_indices[s[ind_prev]]
+                left = seen_indices[char] + 1
             
-            prev_index = seen[c]
-            curCount = i - prev_index
-            for rem_i in range(last_start, prev_index):
-                del seen[s[rem_i]]
-            
-            seen[c] = i
-            last_start = prev_index + 1
+            seen_indices[char] = right
+            maxLen = max(maxLen, right - left + 1)
 
-        return max(curCount, bestCount)
+        return maxLen
+        
