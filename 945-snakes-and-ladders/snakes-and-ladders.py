fd537a3c31b55e0a039641b49681746555dfc561
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2025-05-30 18:25:20 -0700

    Time: 11 ms (95.60%) | Memory: 18 MB (55.61%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.py
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -1,51 +1,34 @@
+from collections import deque
+
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        n = len(board)
-        maxTile = n**2
-        options = [1]
-        seen = set(options)
-        dist = 1
+        # First flatten the board
+        portals = [-1]
+        for row in range(len(board) -1, -1, -1):
+            for col in range(len(board)):
+                if (len(board) - row) % 2 == 0:
+                    col = len(board) - 1 - col
+                portals.append(board[row][col])
+        
+        # Now basic bfs
+        target = len(board) ** 2
+        seen = {1}
+        cur = deque([1])
+        depth = 0
+        while cur:
+            for _ in range(len(cur)):
+                node = cur.popleft()
+                for nextNode in range(node + 1, min(target + 1, node + 7)):
+                    if portals[nextNode] != -1:
+                        nextNode = portals[nextNode]
 
-        while options:
-            #print(options)
-            next_round = []
-            for option in options:
-                for move in range(1, 7):
-                    target = option + move
-                    if (target > maxTile): break
+                    if nextNode == target:
+                        return depth + 1
+                    elif nextNode not in seen:
+                        seen.add(nextNode)
+                        cur.append(nextNode)
 
-                    row = (target - 1) // n
-                    extra = (target - 1) % n
-                    #print(target, "means", row, extra)
-                    
-                    if row % 2 == 1:
-                        jump = board[n - 1 - row][n - 1 - extra]
-                    else:
-                        jump = board[n - 1 - row][extra]
-                    
-                    if jump > -1:
-                        #seen.add(target)
-                        target = jump
-                        
-                    
-                    #print("From", option, move, "can go to", target)
-                    if (target in seen):
-                        #print("Dupe, ignored") 
-                        continue
-
-                    if target == maxTile:
-                        return dist
-
-                    seen.add(target)
-                    next_round.append(target)
-                    
-
-
-
-            options = next_round
-            dist += 1
-
-
+            depth += 1
 
         return -1
         
