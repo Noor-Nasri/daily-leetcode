bdd2ac175cf097c29fe6cadfd960cacc30ac8bbc
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2025-05-30 20:48:38 -0700

    Time: 35 ms (99.23%) | Memory: 22.9 MB (94.39%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -9,21 +9,11 @@ class Solution:
         # len(inds_odd) = 4 k = 3 --> 0, 1
         num_valid = 0
         for i in range(len(inds_odd) - k + 1):
-            if i == 0:
-                ind_of_prev_odd = -1
-            else:
-                ind_of_prev_odd = inds_odd[i - 1]
-
-            ind_of_last_acceptable_odd = inds_odd[i + k - 1]
-
-            if i + k == len(inds_odd):
-                ind_of_next_odd = len(nums)
-            else:
-                ind_of_next_odd = inds_odd[i + k]
-
-            # 0 - (-1) 
+            ind_of_prev_odd = (i == 0) and -1 or inds_odd[i - 1]
+            ind_of_next_odd = (i + k == len(inds_odd)) and len(nums) or inds_odd[i + k]
+            
             valid_start = inds_odd[i] - ind_of_prev_odd
-            valid_end = ind_of_next_odd - ind_of_last_acceptable_odd
+            valid_end = ind_of_next_odd - inds_odd[i + k - 1]
             num_valid += valid_start * valid_end
 
         return num_valid
