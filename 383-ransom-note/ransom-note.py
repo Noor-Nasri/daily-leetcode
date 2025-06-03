bf4699356b404f9f80b9e7a2369a178094a73ce5
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2025-06-02 18:13:55 -0700

    Time: 23 ms (48.60%) | Memory: 17.7 MB (96.48%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counts = [0 for i in range(26)]
+        for c in magazine:
+            counts[ord(c) - ord('a')] += 1
+        
+        for c in ransomNote:
+            counts[ord(c) - ord('a')] -= 1
+            if counts[ord(c) - ord('a')] < 0:
+                return False
+        
+        return True
+
+
+        
