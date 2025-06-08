7dde842569f90b4dac3ce44343dbd7761ff3a9c7
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2025-06-08 15:02:29 -0700

    Time: 15 ms (58.63%) | Memory: 24.5 MB (9.10%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,4 +1,8 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
+        # 1 .. 10 .. 100
+        # then go up til desired. And when hitting a few degit: eg 200, we first want the shorter types
+        # 2, 20, etc.
+        # Too much of a hassle 
         return [int(e) for e in sorted([str(i) for i in range(1, n+1)])]
         
