0217a3f73c051dc9d0341e8ce499ceaf9aa70bbc
Noor Nasri <37914931+Noor-Nasri@users.noreply.github.com>
2026-07-14 21:05:37 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.29%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -0,0 +1,7 @@
+class Solution:
+    # sumEven = n * (n + 1) # First: 2, last: 2*n
+    # sumOdd = n * n 
+    # So then n is a factor in both, and the other has n + 1 lol. So thats GCD.
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
+
